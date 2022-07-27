6084e03f9544844a9de6cce08923122c04053053
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-07-27 12:19:18 +0700

    feat: add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Made with 💚 by Meilika Dwi Putri</p>
+        </div>
+    )
+}
+
+export default Footer;
