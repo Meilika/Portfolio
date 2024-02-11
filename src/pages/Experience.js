2e62a910e53b4cae3b2f76abe3cdce969b143aa0
Meilika <meilika.dwi101619@students.unila.ac.id>
2024-02-11 11:37:25 +0700

    feat: add responsive at experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -7,7 +7,7 @@ export default function MyExperience() {
         <div className="experience">
             <h2>Learning Experience</h2>
             <div className="exp-list"> 
-                <img src="https://media-exp1.licdn.com/dms/image/C560BAQHOIi63tC8k8w/company-logo_100_100/0/1660182933061?e=1675900800&v=beta&t=m4inU8JlglB6JLp9pJqIAAHnJHtaSyL53pv64o5YbJg" alt="Dicoding Indonesia Logo" />
+                <img src="https://gitlab.com/uploads/-/system/group/avatar/9844065/dicoding-logo-square.png" alt="Dicoding Indonesia Logo" />
                 <div class="experience-detail">
                   <a href="https://www.linkedin.com/in/meilika-dwi-putri/details/experience/" target="blank"><h3>Independent Study of Front-End Web Developer and Back-End </h3></a>
                   <p> <b>Dicoding Indonesia</b> - Dicoding Academy (Remote)</p>
@@ -15,7 +15,7 @@ export default function MyExperience() {
                 </div>
             </div>
             <div className="exp-list"> 
-                <img src="https://media-exp1.licdn.com/dms/image/C560BAQEIu6Alk0BuZA/company-logo_100_100/0/1544679117747?e=1675900800&v=beta&t=lud2vZypu4lxe-06sp6P3OkwiOdd8vO4rVeCBi4uv80" alt="Ruangguru Logo" />
+                <img src="https://cdn-images-1.medium.com/max/1200/1*WIznmKuAneDUnNi3t_An1w.png" alt="Ruangguru Logo" />
                 <div class="experience-detail">
                   <a href="https://www.linkedin.com/in/meilika-dwi-putri/details/experience/" target="blank"><h3>Independent Study of Front End Engineering Bootcamp </h3></a>
                   <p> <b>Ruangguru</b> - Ruangguru CAMP (Remote)</p>
